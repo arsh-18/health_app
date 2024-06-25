34f9cd50dff2f57ca5becfd0f41fc78843f6f15a
Arsh Poddar <arshpoddar4@gmail.com>
2024-06-26 01:29:44 +0530

    backend-url-changed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://health-app-backend-7ose.onrender.com";
 
 export const token = localStorage.getItem('token');
 
