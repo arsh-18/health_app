db21509aaf72c2c3285bea82d7c79173830a87dc
Arsh Poddar <arshpoddar4@gmail.com>
2024-06-26 01:34:56 +0530

    Balle balle

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://health-app-backend-7ose.onrender.com";
+export const BASE_URL = "https://health-app-backend-7ose.onrender.com/api/v1";
 
 export const token = localStorage.getItem('token');
 
